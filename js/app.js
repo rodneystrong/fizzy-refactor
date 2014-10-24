47f47a4271f98d95b1ba4b744923964227f1ee42
unknown <akang2@gmail.com>
2014-10-23 20:18:43 -0700

    removed inline js from html and fixed onclick handling in js file

--- js/app.js
+++ js/app.js
@@ -1,36 +1,3 @@
-//grab the area where you want to display the output and set it equal to a var
-
-// var thePlace = $('#the-numbers');
-// //unit test
-// //thePlace.append('hello');
-
-// //create the variable that will house the numbers
-// var theNumber = 0;
-// //console.log(theNumber);
-
-// //create a function that will print out 1 to 100
-// function printNumbers() {
-// 	for(i=0; i<100; i++){
-// 		theNumber++;
-// 		//unit test
-// 		//console.log(theNumber);
-// 		//unit test
-// 		//thePlace.append(theNumber + '<br/>');
-// 		if(theNumber % 3 == 0 && theNumber % 5 == 0){
-// 			thePlace.append('<li class="space">fizz buzz</li>');
-// 		} else if(theNumber % 3 == 0) {
-// 			thePlace.append('<li class="space">fizz</li>');
-// 		} else if(theNumber % 5 == 0) {
-// 			thePlace.append('<li class="space">buzz');
-// 		} else {
-// 			thePlace.append('<li class="space">' + theNumber + "</li>");
-// 		}
-// 	}
-// }
-
-// //call the printNumbers function
-// printNumbers();
-
 
 /*Javascript only version*/
 var thePlace = document.getElementById('the-numbers');
@@ -69,6 +36,4 @@ function printNumber(){
 	}
 }
 
-//printNumber();
-
-//theButton.onclick = printNumber();
+theButton.onclick = printNumber;
